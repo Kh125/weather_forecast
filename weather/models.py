9eb66e68b047e9d4af06ad41aa2c426483f88ba7
Khant Hmuu <khanthmu152uit@gmail.com>
2020-11-10 14:44:14 +0000

    modify design pattern

--- weather/models.py
+++ weather/models.py
@@ -1,5 +1,7 @@
 from weather import db
 
 class City(db.Model):
-  ID = db.Column(db.Integer,primary_key = True)
-  NAME = db.Column(db.String(60),nullable=False)
+  id = db.Column(db.Integer,primary_key = True)
+  name = db.Column(db.String(60),nullable=False,unique=True)
+  def __repr__(self):
+    return f"City('{self.id}','{self.name}')"
--- weather/routes.py
+++ weather/routes.py
@@ -1,23 +1,37 @@
-from flask import render_template,request
-from weather import app
+from flask import render_template, request, flash, redirect, url_for
+from weather import app, db
+from weather.models import City
 import requests
 
-@app.route("/",methods=['GET','POST'])
-@app.route("/home",methods=['GET','POST'])
+
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
 def home():
-  city = 'Yangon'
-  if request.method == 'POST':
-    city = request.form.get('city')
+    city = 'Yangon'
+    if request.method == 'POST':
+        city = request.form.get('city')
 
-  url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=06bb9073587b0c1190b6b110119e6d93' 
-  req = requests.get(url.format(city)).json()
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=06bb9073587b0c1190b6b110119e6d93'
+    req = requests.get(url.format(city)).json()
 
-  weather_data = {
-    'city':city.upper(),
-    'temperature': req['main']['temp'],
-    'desc':req['weather'][0]['description'].capitalize(),
-    'img':req['weather'][0]['icon']
-  }
-  print(weather_data)
+    print(req)
 
-  return render_template("index.html",weather=weather_data)
+    weather_data = {
+        'city': city,
+        'temperature': req['main']['temp'],
+        'desc': req['weather'][0]['description'],
+        'img': req['weather'][0]['icon']
+    }
+    print(weather_data)
+
+    return render_template("index.html", weather=weather_data)
+
+
+@app.route("/add/<string:name>", methods=['POST'])
+def add(name):
+    if request.method == 'POST':
+        c1 = City(name=name)
+        db.session.add(c1)
+        db.session.commit()
+        flash(f'Added to List!', 'success')
+    return redirect(url_for('home'))
